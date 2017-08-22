c1111ebbed4687f6daadf5702a08cce724475038
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    msm8976-common: Build Camera HAL

--- msm8976.mk
+++ msm8976.mk
@@ -117,6 +117,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     camera.msm8952 \
     SnapdragonCamera \
     libcamera_parameters_shim
