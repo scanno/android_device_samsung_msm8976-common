875606c19c5e0e42d15a0a64f9715089bfdd8fe2
Menno van Grinsven <scanno71@gmail.com>
2017-09-16 21:18:00 +0200

    msm8976-common: Remove SnapdragonCamera for now
    Change-Id: Iecbcb8aa3e6c33bc28867222e7b850a731de2adf

--- msm8976.mk
+++ msm8976.mk
@@ -121,7 +121,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
     camera.msm8952 \
-    SnapdragonCamera \
     libcamera_parameters_shim
 
 # DRM
